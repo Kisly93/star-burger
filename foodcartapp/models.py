989b0ee94433ab52020bcd9806eb7a4cbc4923e3
Kisly93 <spsvk.kis.sov@gmail.com>
2023-09-30 19:50:54 +0200

    added payment_method

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.db.models import Sum, F
+from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -144,7 +145,18 @@ class Order(models.Model):
     status = models.PositiveIntegerField(
         verbose_name='Cтатус', choices=STATUS_CHOICES, default=1, db_index=True
     )
+    PAYMENT_CHOICES = (
+        (1, 'Наличные'),
+        (2, 'Электронно'),
+    )
+    payment_method = models.PositiveIntegerField(
+        verbose_name='Способ оплаты', choices=PAYMENT_CHOICES, default=1, db_index=True
+    )
+
     comment = models.TextField(verbose_name='Комментарий', max_length=200, blank=True)
+    registered_at = models.DateTimeField(verbose_name='Время регистрации',default=timezone.now, db_index=True)
+    called_at = models.DateTimeField(verbose_name='Время звонка менеджера', null=True, blank=True, db_index=True)
+    delivered_at = models.DateTimeField(verbose_name='Время доставки', null=True, blank=True, db_index=True)
 
     class Meta:
         verbose_name = 'покупатель'
