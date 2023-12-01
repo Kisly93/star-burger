20e6c174c5319cbcb3aa80871ac6d1246d49d9df
Kisly93 <spsvk.kis.sov@gmail.com>
2023-12-01 19:53:08 +0100

    12

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ YANDEX_GEOCODER_API_KEY = env('YANDEX_GEOCODER_API_KEY')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
